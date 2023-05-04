77d78ca149831584127826a13186eed0719da8ce
sebastian espeset <sebastian.c.espeset@gmail.com>
2023-05-03 22:44:12 -0500

    changes asset imports"

--- src/Releases/Music.jsx
+++ src/Releases/Music.jsx
@@ -1,4 +1,8 @@
 import "./music.scss";
+import apple from "../assets/apple.svg";
+import spotify from "../assets/spotify.png";
+import bandcamp from "../assets/bandcamp.svg";
+import youtube from "../assets/youtube.svg";
 
 export default function Music() {
   return (
@@ -8,23 +12,20 @@ export default function Music() {
       </div>
       <div className="musiccontainer">
         <div className="platformBox">
-          <img
-            src="/assets/Apple_Music_Small_Badge_RGB_091917.svg"
-            alt="apple music link"
-          />
+          <img src={apple} alt="apple music link" />
         </div>
         <div className="platformBox">
-          <img src="/assets/Spotify_Logo_CMYK_White.png" alt="" />
+          <img src={spotify} alt="spotify link" />
         </div>
         <div className="platformBox">
           <img
-            src="/assets/Bandcamp-logotype-color.svg"
-            alt=""
-            style={{ filter: "invert(100%" }}
+            src={bandcamp}
+            alt="bandcamp link"
+            style={{ filter: "invert(100%)" }}
           />
         </div>
         <div className="platformBox">
-          <img src="/assets/YouTube_Logo_2017.svg" alt="" />
+          <img src={youtube} alt="youtube link" />
         </div>
       </div>
     </div>
