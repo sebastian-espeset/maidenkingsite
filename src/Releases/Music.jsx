a40db07c706817d96bcc1e18739873136326800b
sebastian espeset <sebastian.c.espeset@gmail.com>
2023-06-08 13:16:43 -0500

    adds font integration for desktop

--- src/Releases/Music.jsx
+++ src/Releases/Music.jsx
@@ -12,20 +12,53 @@ export default function Music() {
       </div>
       <div className="musiccontainer">
         <div className="platformBox">
-          <img src={apple} alt="apple music link" />
+          <img
+            src={apple}
+            alt="apple music link"
+            onClick={() => {
+              window.open(
+                "https://music.apple.com/us/album/echo/1670366066?i=1670366067",
+                "_blank"
+              );
+            }}
+          />
         </div>
         <div className="platformBox">
-          <img src={spotify} alt="spotify link" />
+          <img
+            src={spotify}
+            alt="spotify link"
+            onClick={() => {
+              window.open(
+                "https://open.spotify.com/artist/2HgKV0kbxHbl3LAsuo9Cb0?si=wSnlILbOTDOFMJlEINSnbQ",
+                "_blank"
+              );
+            }}
+          />
         </div>
         <div className="platformBox">
           <img
             src={bandcamp}
             alt="bandcamp link"
             style={{ filter: "invert(100%)" }}
+            onClick={() => {
+              window.open(
+                "https://maidenking.bandcamp.com/track/echo",
+                "_blank"
+              );
+            }}
           />
         </div>
         <div className="platformBox">
-          <img src={youtube} alt="youtube link" />
+          <img
+            src={youtube}
+            alt="youtube link"
+            onClick={() => {
+              window.open(
+                "https://www.youtube.com/watch?v=3lbI7eDSpz8",
+                "_blank"
+              );
+            }}
+          />
         </div>
       </div>
     </div>
--- src/VideoPlayer/VideoPlayer.jsx
+++ src/VideoPlayer/VideoPlayer.jsx
@@ -5,7 +5,7 @@ export default function VideoPlayer() {
   return (
     <div className="videocontainer" id="video">
       <div className="title">
-        <h1>Who Else Were We Supposed To Become?</h1>
+        <h1>&quot; Who Else Were We Supposed To Become? &quot;</h1>
       </div>
       <video autoPlay muted playsInline src={Untitled}></video>
     </div>
