986705bb68a151ef3508c982f4d9ce58947c3fd5
sebastian espeset <sebastian.c.espeset@gmail.com>
2023-06-12 14:48:31 -0500

    removed video player, adjusted scss as needed

--- src/VideoPlayer/VideoPlayer.jsx
+++ src/VideoPlayer/VideoPlayer.jsx
@@ -1,13 +1,15 @@
 import "./videoPlayer.scss";
-import Untitled from "../assets/Untitled.mp4";
+// import Untitled from "../assets/Untitled.mp4";
 
 export default function VideoPlayer() {
   return (
-    <div className="videocontainer" id="video">
-      <div className="title">
-        <h1>&quot; Who Else Were We Supposed To Become? &quot;</h1>
+    <div className="imagebox">
+      <div className="videocontainer" id="video">
+        <div className="title">
+          <h1>&quot; Who Else Were We Supposed To Become? &quot;</h1>
+        </div>
+        {/* <video autoPlay muted playsInline src={Untitled}></video> */}
       </div>
-      <video autoPlay muted playsInline src={Untitled}></video>
     </div>
   );
 }
